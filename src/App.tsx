c7b25e3f8bb16fc0c1e49fdae5063efb4bb97af4
Yoyler Mosquera <yoyler.mosquera@antpack.co>
2022-06-17 09:24:20 -0500

    Register with basic props, dinamitc inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,167 @@
-
-import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/system';
-import Login from './components/Login';
-import Register from './components/Register';
-import { unstable_createMuiStrictModeTheme } from '@mui/material';
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/system";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { unstable_createMuiStrictModeTheme } from "@mui/material";
+import * as yup from "yup";
 
 const theme = unstable_createMuiStrictModeTheme({
   palette: {
-      primary: {
-      light: '#00ff00',
-      main: '#00ff00',
-      dark: '#002884',
-      contrastText: '#fff',
-      },
-      secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
-      },
+    primary: {
+      light: "#00ff00",
+      main: "#00ff00",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
   },
-  });
+});
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <Login/>
-        {/* <Register/> */}
-      </ThemeProvider>
+
+      {
+        // register nuevo usuario
+      }
+      {/* <>
+        <Register
+          onSubmitForm={(d) => {
+
+
+
+
+            console.log(d);
+          }}
+          requiredTermsAndConditions={true}
+          inputs={[
+            {
+              label: "Nombre",
+              name: "name",
+              type: "text",
+              required: true,
+
+              validation: yup.string().required("Este campo es requerido"),
+            },
+            {
+              label: "Apellido",
+              name: "lasName",
+              type: "text",
+              required: true,
+              validation: yup.string().required(),
+            },
+            {
+              label: "Correo",
+              name: "email",
+              type: "email",
+              required: true,
+              validation: yup.string().required().email(),
+            },
+            {
+              label: "Contraseña",
+              name: "password",
+              type: "password",
+              required: true,
+              validation: yup.string().required(),
+            },
+            {
+              label: "Confirmar contraseña",
+              name: "confirmPassword",
+              type: "password",
+              required: true,
+              validation: yup
+                .string()
+                .required()
+                .oneOf(
+                  [yup.ref("password"), null],
+                  "Las contraseñas deben coincidir"
+                ),
+            },
+            {
+              label: "Tipo de persona",
+              name: "personType",
+              type: "text",
+              required: true,
+              defaulValue: "1",
+              options: [
+                {
+                  label: "NATURAL",
+                  value: "1",
+                },
+                {
+                  label: "JURIDICA",
+                  value: "2",
+                },
+              ],
+              // validation: yup.string().required()
+            },
+            {
+              label: "Tipo de persona",
+              name: "personType",
+              type: "text",
+              required: true,
+              defaulValue: "1",
+              options: [
+                {
+                  label: "NATURAL",
+                  value: "1",
+                },
+                {
+                  label: "JURIDICA",
+                  value: "2",
+                },
+              ],
+              // validation: yup.string().required()
+            },
+          ]}
+        />
+      </> */}
+
+      <>
+        <Register
+          xs={12}
+          onSubmitForm={(d) => {
+            alert(JSON.stringify(d, null, 2))
+          }}
+          requiredTermsAndConditions={false}
+          btnSubmitText={"Cambiar contraseña"}
+          inputs={[
+            {
+              label: "Contraseña anterior",
+              name: "previusPassword",
+              type: "password",
+              required: true,
+              validation: yup.string().required("Este campo es requerido"),
+            },
+            {
+              label: "Nueva Contraseña",
+              name: "password",
+              type: "password",
+              required: true,
+              validation: yup.string().required(),
+            },
+            {
+              label: "Confirmar contraseña",
+              name: "confirmPassword",
+              type: "password",
+              required: true,
+              validation: yup
+                .string()
+                .required()
+                .oneOf(
+                  [yup.ref("password"), null],
+                  "Las contraseñas deben coincidir"
+                ),
+            },
+          ]}
+        />
+      </>
+      <ThemeProvider theme={theme}>{/* <Login/> */}</ThemeProvider>
     </div>
   );
 }
--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,12 +1,169 @@
-import React from 'react'
+import React from "react";
+import { Button, Checkbox, Grid, MenuItem, TextField } from "@mui/material";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from '@mui/material/FormHelperText';
 
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import type { AnySchema } from "yup";
 
-const Register = (data: any) => {
-    return (
-        <>
-            <h1>Hola mundo</h1>
-        </>
-    )
+import styles from "./style.module.css";
+
+interface InputProp {
+  name: string;
+  label: string;
+  type: React.HTMLInputTypeAttribute;
+  required?: boolean;
+  defaulValue?: any;
+  options?: {
+    label: string;
+    value: string;
+  }[]
+  validation?: AnySchema;
 }
 
-export default Register
+interface RegisterProps {
+  inputs?: InputProp[];
+  onSubmitForm?: (data: any) => void;
+  requiredTermsAndConditions?: boolean;
+
+  xs?: number;
+  termsAndConditionsText?: string
+  btnSubmitText?: string
+}
+
+const Register = ({
+  inputs = [],
+  onSubmitForm = (data) => { console.log(data) },
+  requiredTermsAndConditions = true,
+  xs=6,
+  termsAndConditionsText = "Acepto los terminos y condiciones",
+  btnSubmitText = "Registrarse"
+}: RegisterProps) => {
+
+
+  const inputsSchemas = inputs.reduce((acc, inputData) => {
+    return {
+      ...acc,
+      [inputData.name]: inputData.validation,
+    };
+  }, {
+    // si require que se acepten terminos y condiones, se deben validar mediante react hook forms 
+    termsAndConditions: requiredTermsAndConditions ?  yup.boolean().oneOf([true]).required() : yup.boolean()
+  })
+
+  const validationSchema = yup.object({
+    ...inputsSchemas,
+  });
+
+  // console.log(validationSchema)
+
+  const { handleSubmit, formState, control, setValue, getValues  } = useForm({
+    resolver: yupResolver(validationSchema!),
+  });
+
+  // const { _formValues  } = control
+  const { errors,  } = formState;
+
+  const onSubmit = handleSubmit((data) => {
+    onSubmitForm(data);
+  });
+
+    return (
+      <div className={styles.container}>
+        <form onSubmit={onSubmit}>
+          <Grid container rowSpacing={2} columnSpacing={2}>
+            {inputs.map((input, inputIndex) => {
+              const { label, name: nameData, type, required, defaulValue, options = [] } = input;
+              const isSelect = options.length > 0
+              return (
+                <Grid item xs={xs} key={inputIndex}>
+                  <Controller
+                    control={control}
+                    name={nameData}
+                    defaultValue={defaulValue}
+                    render={({ field }) => {
+                      const { name, onBlur, onChange, value } = field;
+                      return (
+                        <TextField
+                          key={name}
+                          required={required}
+                          // placeholder=""
+                          name={name}
+                          label={label}
+                          type={type}
+                          onBlur={onBlur}
+                          value={value}
+                          select={isSelect}
+                          onChange={(e) => {
+                            onChange(e);
+                          }}
+                          fullWidth
+                          error={Boolean(errors[name])}
+                          helperText={errors[name]?.message}
+                        >
+                        {
+                          isSelect && (
+                            options.map(opt => {
+                              return (
+                                <MenuItem value={opt.value} key={value}>
+                                  {opt.label}
+                                </MenuItem>
+                              )
+                            })
+                          )
+                        }
+                        </TextField>
+                      );
+                    }}
+                  />
+                </Grid>
+              );
+            })}
+            {
+              requiredTermsAndConditions && (
+                <Grid item xs={12}>
+                  <Controller
+                    name="termsAndConditions"
+                    control={control}
+                    defaultValue={false}
+                    render={({ field }) => {
+                      return (
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              {...field}
+                              onChange={(e) => {
+                                setValue('termsAndConditions', e.target.checked)
+                              }}
+                              checked={field.value}
+                              required
+                            />
+                          }
+                          label={termsAndConditionsText}
+                        />
+                      );
+                    }}
+                  />
+                </Grid>
+              )
+            }
+          
+            <Grid item xs={12}>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button variant="outlined" type={"submit"}>
+                  {btnSubmitText}
+                </Button>
+              </div>
+            </Grid>
+            {
+              JSON.stringify(getValues(), null, 2)
+            }
+          </Grid>
+        </form>
+      </div>
+    );
+};
+
+export default Register;
