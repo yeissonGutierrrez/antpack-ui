f7377073ce2dcd3d65e2d6b823765eb6aaf12a86
yeisson stik gutierrez <stik@yeissons-MacBook-Air.local>
2022-06-21 15:08:49 -0500

    making login inputs

--- src/App.tsx
+++ src/App.tsx
@@ -25,138 +25,27 @@ const theme = unstable_createMuiStrictModeTheme({
 function App() {
   return (
     <div className="App">
-
-      {
-        // register nuevo usuario
-      }
-      {/* <>
-        <Register
-          onSubmitForm={(d) => {
-
-
-
-
-            console.log(d);
-          }}
-          requiredTermsAndConditions={true}
-          inputs={[
-            {
-              label: "Nombre",
-              name: "name",
-              type: "text",
-              required: true,
-
-              validation: yup.string().required("Este campo es requerido"),
-            },
-            {
-              label: "Apellido",
-              name: "lasName",
-              type: "text",
-              required: true,
-              validation: yup.string().required(),
-            },
-            {
-              label: "Correo",
-              name: "email",
-              type: "email",
-              required: true,
-              validation: yup.string().required().email(),
-            },
-            {
-              label: "Contraseña",
-              name: "password",
-              type: "password",
-              required: true,
-              validation: yup.string().required(),
-            },
-            {
-              label: "Confirmar contraseña",
-              name: "confirmPassword",
-              type: "password",
-              required: true,
-              validation: yup
-                .string()
-                .required()
-                .oneOf(
-                  [yup.ref("password"), null],
-                  "Las contraseñas deben coincidir"
-                ),
-            },
-            {
-              label: "Tipo de persona",
-              name: "personType",
-              type: "text",
-              required: true,
-              defaulValue: "1",
-              options: [
-                {
-                  label: "NATURAL",
-                  value: "1",
-                },
-                {
-                  label: "JURIDICA",
-                  value: "2",
-                },
-              ],
-              // validation: yup.string().required()
-            },
-            {
-              label: "Tipo de persona",
-              name: "personType",
-              type: "text",
-              required: true,
-              defaulValue: "1",
-              options: [
-                {
-                  label: "NATURAL",
-                  value: "1",
-                },
-                {
-                  label: "JURIDICA",
-                  value: "2",
-                },
-              ],
-              // validation: yup.string().required()
-            },
-          ]}
-        />
-      </> */}
-
       <>
-        <Register
+        <Login
           xs={12}
           onSubmitForm={(d) => {
             alert(JSON.stringify(d, null, 2))
           }}
-          requiredTermsAndConditions={false}
           btnSubmitText={"Cambiar contraseña"}
           inputs={[
             {
-              label: "Contraseña anterior",
-              name: "previusPassword",
-              type: "password",
+              label: "Email",
+              name: "email",
+              type: "email",
               required: true,
-              validation: yup.string().required("Este campo es requerido"),
+              validation: yup.string().required("Email requerido"),
             },
             {
-              label: "Nueva Contraseña",
-              name: "password",
+              label: "Contraseña",
+              name: "Password",
               type: "password",
               required: true,
-              validation: yup.string().required(),
-            },
-            {
-              label: "Confirmar contraseña",
-              name: "confirmPassword",
-              type: "password",
-              required: true,
-              validation: yup
-                .string()
-                .required()
-                .oneOf(
-                  [yup.ref("password"), null],
-                  "Las contraseñas deben coincidir"
-                ),
+              validation: yup.string().required("Contraseña requerida"),
             },
           ]}
         />
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,45 +1,170 @@
-import { Box, Card, Grid, Input, Select, TextField } from '@mui/material'
+import { Button, Checkbox, FormControlLabel, Grid, MenuItem, TextField } from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
 import { spacing, ThemeProvider } from '@mui/system';
 import React from 'react'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import type { AnySchema } from "yup";
+import styles from "./style.module.css";
 
 
-const schema = yup.object({
-    firstName: yup.string().matches(/^[A-Za-z]+$/i, 'debe ser caracter valido').required('hola esto es requerido'),
-    age: yup.number().positive().integer().required(),
-  }).required();
+    const emailRex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-const Login = (data: any) => {
-    const { register, handleSubmit, formState:{ errors } } = useForm({
-        resolver: yupResolver(schema)
-      });
-      const onSubmit = (data: any )=> console.log(data);
+    // const schema = yup.object({
+    //     email: yup.string().matches( emailRex, 'debe ser caracter valido').required('hola esto es requerido'),
+    // }).required();
+
+
+    interface InputProp {
+        name: string;
+        label: string;
+        type: React.HTMLInputTypeAttribute;
+        required?: boolean;
+        defaulValue?: any;
+        options?: {
+        label: string;
+        value: string;
+        }[]
+        validation?: AnySchema;
+    }
+
+    interface LoginProps {
+        inputs?: InputProp[];
+        onSubmitForm?: (data: any) => void;
+        saveUser?: boolean;
+        xs?: number;
+        saveUserText?: string
+        btnSubmitText?: string
+    }
+
+    const Login = ({
+        inputs = [],
+        onSubmitForm = (data) => { console.log(data) },
+        saveUser = true,
+        xs=6,
+        saveUserText = "Mantener seccion iniciada",
+        btnSubmitText = "Registrarse"
+    }: LoginProps) => {
+
+    const inputsSchemas = inputs.reduce((acc, inputData) => {
+        return {
+        ...acc,
+        [inputData.name]: inputData.validation,
+        };
+    }, {
+        // si require que se acepten terminos y condiones, se deben validar mediante react hook forms 
+        saveUserText: saveUser ?  yup.boolean().oneOf([true]).required() : yup.boolean()
+    })
+    
+    const validationSchema = yup.object({
+        ...inputsSchemas,
+    });
+    
+    // console.log(validationSchema)
+
+    const { handleSubmit, formState, control, setValue, getValues  } = useForm({
+        resolver: yupResolver(validationSchema!),
+    });
+
+    // const { _formValues  } = control
+    const { errors,  } = formState;
+
+    const onSubmit = handleSubmit((data) => {
+        onSubmitForm(data);
+    });
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Grid container>
-                <Grid xs={12} sm={6}>
-                    <TextField
-                        label='hola mjndo'
-                        fullWidth
-                        {...register("firstName")}
-                        />
-                        <p>{errors.firstName?.message}</p>
+        <div className={styles.container}>
+        <form onSubmit={onSubmit}>
+            <Grid container rowSpacing={2} columnSpacing={2}>
+                {inputs.map((input, inputIndex) => {
+                const { label, name: nameData, type, required, defaulValue, options = [] } = input;
+                const isSelect = options.length > 0
+                return (
+                    <Grid item xs={xs} key={inputIndex}>
+                    <Controller
+                        control={control}
+                        name={nameData}
+                        defaultValue={defaulValue}
+                        render={({ field }) => {
+                        const { name, onBlur, onChange, value } = field;
+                        return (
+                            <TextField
+                                key={name}
+                                required={required}
+                                // placeholder=""
+                                name={name}
+                                label={label}
+                                type={type}
+                                onBlur={onBlur}
+                                value={value}
+                                select={isSelect}
+                                onChange={(e) => {
+                                    onChange(e);
+                                }}
+                                fullWidth
+                                error={Boolean(errors[name])}
+                                helperText={errors[name]?.message}
+                            >
+                                {
+                                isSelect && (
+                                    options.map(opt => {
+                                    return (
+                                        <MenuItem value={opt.value} key={value}>
+                                        {opt.label}
+                                        </MenuItem>
+                                    )
+                                    })
+                                )
+                                }
+                            </TextField>
+                        );
+                        }}
+                    />
+                    </Grid>
+                );
+                })}
+                {
+                saveUser && (
+                    <Grid item xs={12}>
+                    <Controller
+                        name="saveUser"
+                        control={control}
+                        defaultValue={false}
+                        render={({ field }) => {
+                        return (
+                            <FormControlLabel
+                            control={
+                                <Checkbox
+                                {...field}
+                                onChange={(e) => {
+                                    setValue('saveUser', e.target.checked)
+                                }}
+                                checked={field.value}
+                                />
+                            }
+                            label={saveUserText}
+                            />
+                        );
+                        }}
+                    />
+                    </Grid>
+                )
+                }
+            
+                <Grid item xs={12}>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Button variant="outlined" type={"submit"}>
+                        {btnSubmitText}
+                    </Button>
+                </div>
                 </Grid>
-                <Grid xs={12} sm={6}>
-                    <TextField
-                        label='hola mjndo'
-                        fullWidth
-                        {...register("firstName")}
-                        />
-                        <p>{errors.firstName?.message}</p>
-                </Grid>
+                {
+                    JSON.stringify(getValues(), null, 2)
+                }
             </Grid>
-            <TextField type="submit" />
         </form>
-      
+        </div>
     )
 }
 
